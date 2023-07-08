7b62dc4d5bca411e53cf874647bf975d5d32f5c6
thaothaomail <6051071110@st.utc2.edu.vn>
2023-07-08 13:55:35 +0700

    fix

--- FrontEnd/src/firebase.js
+++ FrontEnd/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "translate-app-5e443.appspot.com",
   messagingSenderId: "1010964978359",
   appId: "1:1010964978359:web:8fb0e3b0d92583e7354b32",
-  measurementId: "G-MDH42VTFSH"
+  measurementId: "G-MDH42VTFSH",
 };
 
 firebase.initializeApp(firebaseConfig);
